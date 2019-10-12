fa9a00f71f1b843d2058a5a803ae637be1eaa413
Uche Akotaobi <uche.github@akotaobi.org>
2019-10-12 14:08:13 -0700

    Initial commit for the 2019 Mentor-Bot.
    The new VEX V5 v1.0.x editor makes it easy to set up a controller.
    Stupidly easy, in fact -- I did not write a single line of this code.
    I just pointed and clicked.
    
    That bothers me, so I'll write a PROs equivalent, too.

---
+++ 2019/mentor-bot/src/main.cpp
@@ -0,0 +1,25 @@
+/*----------------------------------------------------------------------------*/
+/*                                                                            */
+/*    Module:       main.cpp                                                  */
+/*    Author:       C:\Users\Test User                                        */
+/*    Created:      Sat Oct 12 2019                                           */
+/*    Description:  V5 project                                                */
+/*                                                                            */
+/*----------------------------------------------------------------------------*/
+
+// ---- START VEXCODE CONFIGURED DEVICES ----
+// Robot Configuration:
+// [Name]               [Type]        [Port(s)]
+// elevatorMotor        motor         1               
+// Controller1          controller                    
+// ---- END VEXCODE CONFIGURED DEVICES ----
+
+#include "vex.h"
+
+using namespace vex;
+
+int main() {
+  // Initializing Robot Configuration. DO NOT REMOVE!
+  vexcodeInit();
+  
+}
