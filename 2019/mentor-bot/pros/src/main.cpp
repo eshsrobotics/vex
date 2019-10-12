729564a7a61cb7dbd26b8e0328591dcdd0029e1c
Uche Akotaobi <uche.github@akotaobi.org>
2019-10-12 16:20:58 -0700

    I'm reading up on the Okapi API, so tthis commit isn't done.
    I'm just saving because I'm about to leave.

--- 2019/mentor-bot/pros/src/main.cpp
+++ 2019/mentor-bot/pros/src/main.cpp
@@ -75,15 +75,14 @@ void autonomous() {}
  */
 void opcontrol() {
 	pros::Controller master(pros::E_CONTROLLER_MASTER);
-	pros::Motor left_mtr(1);
-	pros::Motor right_mtr(2);
+	pros::Motor elevatorMotor(1, E_MOTOR_GEARSET_18, false);
 
 	while (true) {
-		pros::lcd::print(0, "%d %d %d", (pros::lcd::read_buttons() & LCD_BTN_LEFT) >> 2,
-		                 (pros::lcd::read_buttons() & LCD_BTN_CENTER) >> 1,
-		                 (pros::lcd::read_buttons() & LCD_BTN_RIGHT) >> 0);
-		int left = master.get_analog(ANALOG_LEFT_Y);
-		int right = master.get_analog(ANALOG_RIGHT_Y);
+		// pros::lcd::print(0, "%d %d %d", (pros::lcd::read_buttons() & LCD_BTN_LEFT) >> 2,
+		                 // (pros::lcd::read_buttons() & LCD_BTN_CENTER) >> 1,
+		                 // (pros::lcd::read_buttons() & LCD_BTN_RIGHT) >> 0);
+		// int left = master.get_analog(ANALOG_LEFT_Y);
+		// int right = master.get_analog(ANALOG_RIGHT_Y);
 
 		left_mtr = left;
 		right_mtr = right;
