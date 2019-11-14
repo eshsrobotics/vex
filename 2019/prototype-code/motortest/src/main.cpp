2673e483bf8bfbba2ae5e8b738602ff7bac7a519
the-cooler-jackson <jchartier_23@esusdstudents.org>
2019-11-13 17:44:49 -0800

    This code is about testing the forward and back function of the competition bot and the velocity needed for the drivetrain.

---
+++ 2019/prototype-code/motortest/src/main.cpp
@@ -0,0 +1,21 @@
+/*----------------------------------------------------------------------------*/
+/*                                                                            */
+/*    Module:       main.cpp                                                  */
+/*    Author:       C:\Users\jwilsoniii_23                                    */
+/*    Created:      Wed Nov 13 2019                                           */
+/*    Description:  V5 project                                                */
+/*                                                                            */
+/*----------------------------------------------------------------------------*/
+
+// ---- START VEXCODE CONFIGURED DEVICES ----
+// ---- END VEXCODE CONFIGURED DEVICES ----
+
+#include "vex.h"
+
+using namespace vex;
+
+int main() {
+  // Initializing Robot Configuration. DO NOT REMOVE!
+  vexcodeInit();
+  
+}
