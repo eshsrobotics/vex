69c611a2682a1302ff79227c9fb3244b58bcc769
the-cooler-jackson <jcharter_23@esusdstudents.org>
2019-11-17 14:19:23 -0800

    Started programming the control for both lifts and pusher.
    As a progamming team we had agreed to set the following controls for the
    compentition bot. These are are subject to change, and are based on a
    gaming scheme.

--- 2019/prototype-code/motortest/src/main.cpp
+++ 2019/prototype-code/motortest/src/main.cpp
@@ -74,6 +74,19 @@
 // RightIntake          motor         6               
 // IntakeLift           motor         7               
 // TrayPusher           motor         8               
+// Controller1          controller                    
+// ---- END VEXCODE CONFIGURED DEVICES ----
+// ---- START VEXCODE CONFIGURED DEVICES ----
+// Robot Configuration:
+// [Name]               [Type]        [Port(s)]
+// FrontRightWheel      motor         1               
+// FrontLeftWheel       motor         2               
+// BackRightWheel       motor         3               
+// BackLeftWheel        motor         4               
+// LeftIntake           motor         5               
+// RightIntake          motor         6               
+// IntakeLift           motor         7               
+// TrayPusher           motor         8               
 // ---- END VEXCODE CONFIGURED DEVICES ----
 // ---- START VEXCODE CONFIGURED DEVICES ----
 // Robot Configuration:
@@ -153,4 +166,14 @@ int main() {
   // Initializing Robot Configuration. DO NOT REMOVE!
   vexcodeInit();
   
+  while(true) {
+    // allows controller to control both intakes with one button (the left trigger)
+    //left intake is automatically configured with robot config
+    int v = LeftIntake.velocity(percent);
+    RightIntake.setVelocity(v, percent);
+
+    
+
+  
+  }
 }
