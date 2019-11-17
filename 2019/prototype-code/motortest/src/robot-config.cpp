e649a5565a23c50443da0e1b94e3f57043d18d3c
uakotaobi <uche.gitub@akotaobi.org>
2019-11-17 15:52:36 -0800

    Use the Vex V5 wizard to re-assign the left trigger and X/B keypad buttons.
    Everything is designed to obey the motor and port scheme we committed
    earlier today, but we might need to reverse a couple of motors.

--- 2019/prototype-code/motortest/src/robot-config.cpp
+++ 2019/prototype-code/motortest/src/robot-config.cpp
@@ -20,24 +20,49 @@ controller Controller1 = controller(primary);
 
 // VEXcode generated functions
 // define variables used for controlling motors based on controller inputs
+bool Controller1LeftShoulderControlMotorsStopped = true;
 bool Controller1RightShoulderControlMotorsStopped = true;
+bool Controller1XBButtonsControlMotorsStopped = true;
 
 // define a task that will handle monitoring inputs from Controller1
 int rc_auto_loop_callback_Controller1() {
   // process the controller input every 20 milliseconds
   // update the motors based on the input values
   while(true) {
+    // check the ButtonL1/ButtonL2 status to control LeftIntake
+    if (Controller1.ButtonL1.pressing()) {
+      LeftIntake.spin(reverse);
+      Controller1LeftShoulderControlMotorsStopped = false;
+    } else if (Controller1.ButtonL2.pressing()) {
+      LeftIntake.spin(forward);
+      Controller1LeftShoulderControlMotorsStopped = false;
+    } else if (!Controller1LeftShoulderControlMotorsStopped) {
+      LeftIntake.stop();
+      // set the toggle so that we don't constantly tell the motor to stop when the buttons are released
+      Controller1LeftShoulderControlMotorsStopped = true;
+    }
     // check the ButtonR1/ButtonR2 status to control IntakeLift
     if (Controller1.ButtonR1.pressing()) {
-      IntakeLift.spin(forward);
+      IntakeLift.spin(reverse);
       Controller1RightShoulderControlMotorsStopped = false;
     } else if (Controller1.ButtonR2.pressing()) {
-      IntakeLift.spin(reverse);
+      IntakeLift.spin(forward);
       Controller1RightShoulderControlMotorsStopped = false;
     } else if (!Controller1RightShoulderControlMotorsStopped) {
       IntakeLift.stop();
       // set the toggle so that we don't constantly tell the motor to stop when the buttons are released
       Controller1RightShoulderControlMotorsStopped = true;
+    }
+    // check the X/B buttons status to control TrayPusher
+    if (Controller1.ButtonX.pressing()) {
+      TrayPusher.spin(forward);
+      Controller1XBButtonsControlMotorsStopped = false;
+    } else if (Controller1.ButtonB.pressing()) {
+      TrayPusher.spin(reverse);
+      Controller1XBButtonsControlMotorsStopped = false;
+    } else if (!Controller1XBButtonsControlMotorsStopped){
+      TrayPusher.stop();
+      Controller1XBButtonsControlMotorsStopped = true;
     }
     // wait before repeating the process
     wait(20, msec);
