fb3e074a35d351b7df703d9e9af71849b024317e
uakotaobi <uche.github@akotaobi.org>
2019-12-07 12:27:11 -0800

    Adding totally untested code to retrofit a competition template into the existing code.
    If this works, you will need to run the program in competition mode from now on.

--- 2019/prototype-code/motortest/src/main.cpp
+++ 2019/prototype-code/motortest/src/main.cpp
@@ -41,9 +41,75 @@ void mecanumDrive(int leftRight, int forwardBack, int turn) {
   BackLeftWheel.spin(forward,   multiplier * (forwardBack + turn - leftRight), percent);
 }
 
-int main() {
+
+/*----------------------------------------------------------------------------*/
+/*                                                                            */
+/*    Module:       main.cpp                                                  */
+/*    Author:       VEX                                                       */
+/*    Created:      Thu Sep 26 2019                                           */
+/*    Description:  Competition Template                                      */
+/*                                                                            */
+/*----------------------------------------------------------------------------*/
+
+// ---- START VEXCODE CONFIGURED DEVICES ----
+// ---- END VEXCODE CONFIGURED DEVICES ----
+
+#include "vex.h"
+
+using namespace vex;
+
+// A global instance of competition
+competition Competition;
+
+// define your global instances of motors and other devices here
+
+/*---------------------------------------------------------------------------*/
+/*                          Pre-Autonomous Functions                         */
+/*                                                                           */
+/*  You may want to perform some actions before the competition starts.      */
+/*  Do them in the following function.  You must return from this function   */
+/*  or the autonomous and usercontrol tasks will not be started.  This       */
+/*  function is only called once after the V5 has been powered on and        */
+/*  not every time that the robot is disabled.                               */
+/*---------------------------------------------------------------------------*/
+
+void pre_auton(void) {
   // Initializing Robot Configuration. DO NOT REMOVE!
   vexcodeInit();
+
+  // All activities that occur before the competition starts
+  // Example: clearing encoders, setting servo positions, ...
+}
+
+/*---------------------------------------------------------------------------*/
+/*                                                                           */
+/*                              Autonomous Task                              */
+/*                                                                           */
+/*  This task is used to control your robot during the autonomous phase of   */
+/*  a VEX Competition.                                                       */
+/*                                                                           */
+/*  You must modify the code to add your own robot specific commands here.   */
+/*---------------------------------------------------------------------------*/
+
+void autonomous(void) {
+  // ..........................................................................
+  // Insert autonomous user code here.
+  // ..........................................................................
+}
+
+/*---------------------------------------------------------------------------*/
+/*                                                                           */
+/*                              User Control Task                            */
+/*                                                                           */
+/*  This task is used to control your robot during the user control phase of */
+/*  a VEX Competition.                                                       */
+/*                                                                           */
+/*  You must modify the code to add your own robot specific commands here.   */
+/*---------------------------------------------------------------------------*/
+
+void usercontrol(void) {
+  // User control code here, inside the loop
+
   // Unlike our other motors, the IntakeLift needs to maintain its position by
   // expending electrical power.  So we're not just going to set a speed and cut
   // off power when controls are released; instead, we adjust *this* variable,
@@ -59,6 +125,9 @@ int main() {
   LeftIntake.setVelocity(90, percent);
   RightIntake.setVelocity(90, percent);
 
+  // This is the main execution loop for the user control program.
+  // Each time through the loop your program should update motor + servo
+  // values based on feedback from the joysticks.
   while (true) {
     // allows controller to control both intakes with one button (the left
     // trigger)
@@ -140,6 +209,22 @@ int main() {
       //   positionIncrement = 0;
       // }
     }    
-    
+    wait(20, msec); // Sleep the task for a short amount of time to
+                    // prevent wasted resources.    
   } // end (loop forever)
+}
+
+//
+// Main will set up the competition functions and callbacks.
+//
+int main() {
+  // Initializing Robot Configuration. DO NOT REMOVE!
+  vexcodeInit();
+
+  // Set up callbacks for autonomous and driver control periods.
+  Competition.autonomous(autonomous);
+  Competition.drivercontrol(usercontrol);
+
+  // Run the pre-autonomous function.
+  pre_auton();
 }
