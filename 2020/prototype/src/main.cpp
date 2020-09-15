b04c51c2cf20694b4716d6e27084acb45ec51d7e
jimmythepaprika <jwilsoniii_23@esusdstudents.org>
2020-09-14 18:06:57 -0700

    Just created the vex code for the prototype bot for 2020-2021.

---
+++ 2020/prototype/src/main.cpp
@@ -0,0 +1,21 @@
+/*----------------------------------------------------------------------------*/
+/*                                                                            */
+/*    Module:       main.cpp                                                  */
+/*    Author:       C:\Users\Wilson Family                                    */
+/*    Created:      Mon Sep 14 2020                                           */
+/*    Description:  V5 project                                                */
+/*                                                                            */
+/*----------------------------------------------------------------------------*/
+
+// ---- START VEXCODE CONFIGURED DEVICES ----
+// ---- END VEXCODE CONFIGURED DEVICES ----
+
+#include "vex.h"
+
+using namespace vex;
+
+int main() {
+  // Initializing Robot Configuration. DO NOT REMOVE!
+  vexcodeInit();
+  
+}
