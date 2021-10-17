aaeee9998f35f7b0f12b3a94e1b7f71cada37de5
mgookin24 <mgookin_24@esusdstudents.org>
2021-10-17 14:34:02 -0700

    Finished the arms() function for autonomous, added .DS_Store to .gitignore
    - Arms theoretically stop when bump sensors are pressed.
    - Added Matthew's autonomous idea to the program, haven't tested yet.
    - Added .DS_Store to .gitignore because it showed up on Mac.

--- 2021/team-p/Drive_code/src/main.cpp
+++ 2021/team-p/Drive_code/src/main.cpp
@@ -11,10 +11,24 @@
 // ---- END VEXCODE CONFIGURED DEVICES ----
 
 #include "vex.h"
+#include <cmath>
 
 using namespace vex;
+using std::abs;
 int speed = 50;
-int speedSlow = speed/2;
+int speedSlow = speed / 2;
+
+// Returns the sign of a number: -1 for negative numbers, 1 for positive
+// numbers, 0 otherwise
+double sgn(double n) {
+  if (n > 0) {
+    return 1;
+  } else if (n < 0) {
+    return -1;
+  } else {
+    return 0;
+  }
+}
 
 // A global instance of competition
 competition Competition;
@@ -41,51 +55,101 @@ void pre_auton(void) {
 
 // Autonomous and Skills Challenge Support Routines.
 
+bool is_robot_driving() { return Drivetrain.isMoving(); }
 
-bool is_robot_driving() {
-    return Drivetrain.isMoving();
-}
+bool is_arm_moving() { return (ArmLeft.isSpinning() || ArmRight.isSpinning()); }
 
-bool is_arm_moving() {
-  return (ArmLeft.isSpinning() || ArmRight.isSpinning());
-}
+// driveFor(forward, x, mm) might not drive that exact number of millimeters
 
-// Drives the robot a certain number of millimeters forward (+) or backward (-).
-// Driving operation is asynchronous.
-// The motors will continue running after the function ends.
-void drive(double distance_mm) {
-  if (distance_mm > 0) {
-    Drivetrain.driveFor(fwd, distance_mm, mm, false);
-  } else if (distance_mm < 0) {
-    Drivetrain.driveFor(reverse, distance_mm, mm, false);
-  } else {
-    Drivetrain.stop();
-  }
-}
 
 // Moves the arms a certain number of degrees up (+) or down (-)
-// NEED TO FIX:
-// - The push sensors don't stop the arms as they're moving, they can only keep the arms from starting.
-void arms(double dist_degrees) {
-  if (dist_degrees > 0) {
-    ArmRight.rotateFor(forward, (dist_degrees * 15), degrees, false);
-    ArmLeft.rotateFor(forward, (dist_degrees * 15), degrees);
-  } else if (dist_degrees < 0) {
-    if (!LeftArmBumper.pressing()) {
-      ArmLeft.rotateFor(forward, (dist_degrees * 15), degrees, false);
-    } else {
-      ArmLeft.stop(hold);
+// PROBLEM: IF THE ARMS ARE BLOCKED BY ANYTHING WHEN IT IS GOING UP, THE
+// LOOP WILL CONTINUE FOREVER.
+void arms(double dist_degrees, double timeout_msec = 5000) {
+  // Takes the gear ratio into account to move the arms by the
+  // correct amount of degrees.
+  const double GEAR_RATIO = 15;
+  dist_degrees *= GEAR_RATIO;
+
+  bool done = false;
+  // These variables help the arms move to the desired rotation by
+  // accelerating, instead of starting at a certain speed
+  // We do not have anything in the function to help with decelerating
+  const double MAXIMUM_ARM_LIFT_PERCENT = 25;
+  const double ARM_LIFT_ACCELERATION = 0.1;
+  double currentVelocityPercent = 0;
+
+  // The arms should be able to stop independently, instead of both arms
+  // stopping at the same time,so we need two variables
+  bool leftArmSpinning = true;
+  bool rightArmSpinning = true;
+
+  // These variables tell us the starting position for the arms, so we
+  // know when the motors should stop spinning.
+  // Independent of the bump sensors.
+  const double STARTING_ROTATION_LEFT = ArmLeft.rotation(deg);
+  const double STARTING_ROTATION_RIGHT = ArmRight.rotation(deg);
+
+  // If the motors get stuck, we need the starting time to make an
+  // emergency timeout.
+  const double STARTING_TIME_MSEC = Brain.timer(msec);
+
+  // The while loop will block the robot until it is done
+  // This is not an asynchronous function
+  while (!done) {
+    // forward means up, unless given a negative value.
+    if (leftArmSpinning) {
+      ArmLeft.spin(forward, currentVelocityPercent, pct);
     }
-    if (!RightArmBumper.pressing()) {
-      ArmRight.rotateFor(forward, (dist_degrees * 15), degrees);
+    if (rightArmSpinning) {
+      ArmRight.spin(forward, currentVelocityPercent, pct);
+    }
+
+    // Accelerate in the correct direction.
+    if (dist_degrees > 0) {
+      currentVelocityPercent += ARM_LIFT_ACCELERATION;
     } else {
-      ArmRight.stop(hold);
+      currentVelocityPercent -= ARM_LIFT_ACCELERATION;
+    }
+    if (abs(currentVelocityPercent) > MAXIMUM_ARM_LIFT_PERCENT) {
+      currentVelocityPercent =
+          sgn(currentVelocityPercent) * MAXIMUM_ARM_LIFT_PERCENT;
+    }
+
+    // Tests if either the RightArmBumper or the LeftArmBumper is being
+    // pressed, and if so, stops the arms.
+    if (LeftArmBumper.pressing() && dist_degrees < 0) {
+      leftArmSpinning = false;
+    }
+    if (RightArmBumper.pressing() && dist_degrees < 0) {
+      rightArmSpinning = false;
+    }
+
+    // Stops rotating the arm once rotated the desired degrees.
+    if (abs(ArmLeft.rotation(deg) - STARTING_ROTATION_LEFT) >
+        abs(dist_degrees)) {
+      leftArmSpinning = false;
+    }
+    if (abs(ArmRight.rotation(deg) - STARTING_ROTATION_RIGHT) >
+        abs(dist_degrees)) {
+      rightArmSpinning = false;
+    }
+
+    // If both arms are not spinning, or if the time running has
+    // exceeded the timeout parameter, the while loop will end.
+    if ((!leftArmSpinning && !rightArmSpinning) ||
+        (Brain.timer(msec) - STARTING_TIME_MSEC > timeout_msec)) {
+      done = true;
     }
   }
-  else {
-  ArmLeft.stop(hold);
-  ArmRight.stop(hold);
-  }
+
+  // Stops the robot's arms from moving.
+  // brakeType brake stops the arms using power, then cuts power.
+  // brakeType coast just cuts power.
+  // brakeType hold uses power to hold the arms in place.
+  // Used brake to save power, but stop the arm.
+  ArmLeft.stop(brake);
+  ArmRight.stop(brake);
 }
 /*---------------------------------------------------------------------------*/
 /*                                                                           */
@@ -101,13 +165,44 @@ void autonomous(void) {
   // ..........................................................................
   // Insert autonomous user code here.
   // ..........................................................................
+  
+  // Changes the drive and turn velocity for the robot
   Drivetrain.setDriveVelocity(50, pct);
   Drivetrain.setTurnVelocity(50, pct);
-  // arms(-90);
-  // Drivetrain.turnFor(right, 15, degrees);
-  // Drivetrain.driveFor(forward, 410, mm);
-  // arms(-70);
-  // Drivetrain.driveFor(reverse, 250, mm);
+  
+  // Puts a ring in the team mobile goal, then moves the mobile goal off
+  // of the win point line.
+  Drivetrain.turnFor(right, 15, degrees);
+  Drivetrain.driveFor(forward, 410, mm);
+  arms(-70);
+  Drivetrain.driveFor(reverse, 250, mm);
+  arms(-100);
+  Drivetrain.turnFor(left, 25, degrees);
+  Drivetrain.driveFor(forward, 400, mm);
+  arms(16.7);
+  Drivetrain.driveFor(reverse, 250, mm);
+  Drivetrain.turnFor(right, 100, degrees);
+  Drivetrain.driveFor(forward, 175, mm);
+  arms(-16.7);
+  Drivetrain.driveFor(reverse, 400, mm);
+  
+  // Picks up one of the short mobile goals and takes it back to the team's side.
+  Drivetrain.turnFor(left, 150, degrees);
+  Drivetrain.driveFor(forward, 900, mm);
+  arms(16.7);
+  Drivetrain.driveFor(reverse, 850, mm);
+  Drivetrain.turnFor(right, 60, degrees);
+  arms(-16.7);
+  Drivetrain.driveFor(reverse, 250, mm);
+  
+  // Picks up the tallest mobile goal and moves it to the team's side.
+  Drivetrain.turnFor(left, 90, degrees);
+  Drivetrain.driveFor(forward, 1500, mm);
+  Drivetrain.turnFor(right, 180, degrees);
+  Drivetrain.driveFor(forward, 1500, mm);
+  arms(-16.7);
+  Drivetrain.driveFor(reverse, 250, mm);
+  Drivetrain.turnFor(left, 135, degrees);
 }
 
 /*---------------------------------------------------------------------------*/
@@ -131,35 +226,32 @@ void usercontrol(void) {
     // Insert user code here. This is where you use the joystick values to
     // update your motors, etc.
     // ........................................................................
-    if(Controller1.ButtonR1.pressing()){
+    if (Controller1.ButtonR1.pressing()) {
       ArmLeft.spin(forward, speed, pct);
       ArmRight.spin(forward, speed, pct);
-    }
-    else if(Controller1.ButtonR2.pressing()){
-      if(!LeftArmBumper.pressing())
-      ArmLeft.spin(reverse, speed, pct);
-      if(!RightArmBumper.pressing())
-      ArmRight.spin(reverse, speed, pct);
-    }
-    else {
-    ArmLeft.stop(hold);
-    ArmRight.stop(hold);
+    } else if (Controller1.ButtonR2.pressing()) {
+      if (!LeftArmBumper.pressing())
+        ArmLeft.spin(reverse, speed, pct);
+      if (!RightArmBumper.pressing())
+        ArmRight.spin(reverse, speed, pct);
+    } else {
+      ArmLeft.stop(hold);
+      ArmRight.stop(hold);
     }
 
-    //Controls the left shoulder buttons to make the arms go a slower speed (25%)
-    if(Controller1.ButtonL1.pressing()){
+    // Controls the left shoulder buttons to make the arms go a slower speed
+    // (25%)
+    if (Controller1.ButtonL1.pressing()) {
       ArmLeft.spin(forward, speedSlow, pct);
       ArmRight.spin(forward, speedSlow, pct);
-    }
-    else if(Controller1.ButtonL2.pressing()){
-      if(!LeftArmBumper.pressing())
-      ArmLeft.spin(reverse, speedSlow, pct);
-      if(!RightArmBumper.pressing())
-      ArmRight.spin(reverse, speedSlow, pct);
-    }
-    else {
-    ArmLeft.stop(hold);
-    ArmRight.stop(hold);
+    } else if (Controller1.ButtonL2.pressing()) {
+      if (!LeftArmBumper.pressing())
+        ArmLeft.spin(reverse, speedSlow, pct);
+      if (!RightArmBumper.pressing())
+        ArmRight.spin(reverse, speedSlow, pct);
+    } else {
+      ArmLeft.stop(hold);
+      ArmRight.stop(hold);
     }
 
     wait(20, msec); // Sleep the task for a short amount of time to
