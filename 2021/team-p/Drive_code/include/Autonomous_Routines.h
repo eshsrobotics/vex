7538255b9ea4730c51802a918825f0622fc055ab
the-cooler-jackson <shark2016mellon@gmail.com>
2021-11-14 19:29:56 -0800

    We implemented the child parent connection for the Task Class
    Notes:
    -Implemented a shared pointer instead of a unique pointer for the
    children becuase we do not want the parents sharing children to
    be stealing reasources from one another
    
    -Used a weak pointer instead of a shared pointer for the parents becuase
    we want to break shared pointer cycles (otherwise we might leak memory)

--- 2021/team-p/Drive_code/include/Autonomous_Routines.h
+++ 2021/team-p/Drive_code/include/Autonomous_Routines.h
@@ -1,33 +1,43 @@
-#include <memory>  // unique_ptr<T>
+#include <memory>  // shared_ptr<T>
 #include <vector>
 #include <string>
 
 struct Task {
   // Task name, not unique whatsoever
   std::string name;
+
   // Task ID, unique for every task
   int task_id;
+
   // The parent tasks for the current task, the task runs when all parents are finished
-  std::vector<std::unique_ptr<Task>> parents;
+  std::vector<std::weak_ptr<Task>> parents;
+
   // The child tasks of the current task, run when the task is finished
-  std::vector<std::unique_ptr<Task>> children;
+  std::vector<std::shared_ptr<Task>> children;
+
   // Task parameter, the amount to do the task
   double parameter;
 
   // Returns true if the task is completed, false if not completed
   virtual bool done() const = 0;
+
   // Starts the current task
   virtual void start() = 0;
 
   // Add a new or existing task as a child of the current task
-  void addTask(std::unique_ptr<Task> task);
+  friend void addTask(std::shared_ptr<Task> parentTask, 
+                      std::shared_ptr<Task> childTask);
 
   // Generates the unique task ID
-  int makeUniqueID() const;
+  Task();
 };
 
 // Delays other tasks from starting
 struct WaitMillisecondsTask : public Task {
+  double startTimeMilliseconds;
+  double doneTimeMilliseconds;
+  double waitPeriodMilliseconds;  
+
   WaitMillisecondsTask(double milliseconds);
 
   bool done() const;
--- 2021/team-p/Drive_code/src/Autonomous_Routines.cpp
+++ 2021/team-p/Drive_code/src/Autonomous_Routines.cpp
@@ -1,3 +1,33 @@
 #include "vex.h"
+#include "Autonomous_Routines.h"
 
 using namespace vex;
+using namespace std;
+
+
+Task::Task() {
+  static int counter = 0;
+  task_id = ++counter;
+}
+
+void addTask(std::shared_ptr<Task> parentTask, std::shared_ptr<Task> childTask) {
+  parentTask->children.push_back(childTask);
+  childTask->parents.push_back(parentTask);
+}
+
+WaitMillisecondsTask::WaitMillisecondsTask(double milliseconds) : Task() {
+  waitPeriodMilliseconds = milliseconds;
+}
+
+bool WaitMillisecondsTask::done() const {
+  if (Brain.timer(msec) >= doneTimeMilliseconds) {
+    return true;
+  } else {
+    return false;
+  }
+}
+
+void WaitMillisecondsTask::start() {
+  startTimeMilliseconds = Brain.timer(msec);
+  doneTimeMilliseconds = startTimeMilliseconds + waitPeriodMilliseconds;
+}
