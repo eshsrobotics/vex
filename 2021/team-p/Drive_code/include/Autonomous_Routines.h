2a8cd432027341e05c06d042b9c8287ea16c66cb
Matthew Gookin <matthewgookin@Williams-iMac.local>
2021-11-21 14:46:32 -0800

    Added the execute function and driveStraightTask struct for the task tree
    - The execute function starts the task tree by running the first
      task, which runs all of the rest of the tasks
    - The DriveStraightTask struct will drive the robot for a certain number
      of inches forward or backward, depending on the sign
    - If there is a turning task and a driving task running at the same
      time, when one ends, the other one will end even if it isn't finished
    - Added functions to test the task tree, if run, the robot should drive
      forward for 5 inches, wait 2 seconds, then drive backward 5 inches

--- 2021/team-p/Drive_code/include/Autonomous_Routines.h
+++ 2021/team-p/Drive_code/include/Autonomous_Routines.h
@@ -3,6 +3,13 @@
 #include <string>
 
 struct Task {
+  // Generates the unique task ID
+  Task();
+
+  /*--------------------------------------------------------*/
+  /*                     Member data                        */
+  /*--------------------------------------------------------*/
+
   // Task name, not unique whatsoever
   std::string name;
 
@@ -18,19 +25,32 @@ struct Task {
   // Task parameter, the amount to do the task
   double parameter;
 
+  /*--------------------------------------------------------*/
+  /*                  Virtual Methods                       */
+  /*--------------------------------------------------------*/
+
   // Returns true if the task is completed, false if not completed
   virtual bool done() const = 0;
 
   // Starts the current task
   virtual void start() = 0;
 
+  /*--------------------------------------------------------*/
+  /*                  Public Functions                      */
+  /*--------------------------------------------------------*/
+
   // Add a new or existing task as a child of the current task
   friend void addTask(std::shared_ptr<Task> parentTask, 
                       std::shared_ptr<Task> childTask);
 
-  // Generates the unique task ID
-  Task();
+  // Executes this task, all of its children, and so on, some tasks may run in parallel
+  friend void execute(std::shared_ptr<Task> rootTask);
 };
+
+extern void addTask(std::shared_ptr<Task> parentTask, 
+                    std::shared_ptr<Task> childTask);
+
+extern void execute(std::shared_ptr<Task> rootTask);
 
 // Delays other tasks from starting
 struct WaitMillisecondsTask : public Task {
@@ -40,6 +60,21 @@ struct WaitMillisecondsTask : public Task {
 
   WaitMillisecondsTask(double milliseconds);
 
+  bool done() const;
+  void start();
+};
+
+
+struct DriveStraightTask : public Task {
+  vex::drivetrain& drivetrain;
+  double distanceInches;
+
+  // If a negative number is passed in, it will drive backwards instead of forwards
+  DriveStraightTask(vex::drivetrain& drivetrain, double distanceInches);
+
+  // Returns true is the drivetrain has stopped moving
+  // WARNING: Also applies if there is a turning task and a driving task executing at the same 
+  // time, they will both end when one of them finishes, we can't do anything to fix that for now
   bool done() const;
   void start();
 };
