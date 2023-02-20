70f51e4e1075523a2a9c8cc857be87896f78c6ed
fireblaze2000 <mary.amara0307@gmail.com>
2023-02-19 17:27:04 -0800

    Finished the quick and dirty Python script.
    The script seems to work correctly, provided my interpretation of
    the results is accurate.  I could add an ASCII renderer here, but I
    won't.
    
    I formatted the script by hand.  I was able to get YAPF to stop
    line-breaking on opening parentheses, but it still does it for closing
    parentheses, and there are even _fewer_ knobs to tweak there.
    
    I will find better tools.

--- frc-prototypes/2022-2023/frc-robot-arm-inverse-kinematics/calculate.py
+++ frc-prototypes/2022-2023/frc-robot-arm-inverse-kinematics/calculate.py
@@ -7,64 +7,85 @@ verbosity = False
 
 
 def parseArguments():
-    """
+  """
     Parses the command-line arguments and returns our four critical
     parameters: X, Y, L1, and L2.
 
     Returns:
       Returns a four-tuple of (x, y, l1, l2).
     """
-    parser = argparse.ArgumentParser(prog="calculate.py")
+  parser = argparse.ArgumentParser(prog="calculate.py")
 
-    parser.add_argument(
-        "X",
-        type=float,
-        help="Base of the right triangle; outward extent of the arm.")
-    parser.add_argument(
-        "Y",
-        type=float,
-        help="Height of the right triangle; upward extent of the arm.")
-    parser.add_argument(
-        "L1",
-        type=float,
-        help="Length of the upper arm (shoulder to elbow) in inches.")
-    parser.add_argument(
-        "L2",
-        type=float,
-        help="Length of the forearm (elbow to wrist), again in inches.")
-    parser.add_argument(
-        "-v",
-        "--verbose",
-        type=bool,
-        default=False,
-        help="If supplied, causes this script to print more verbose messages.")
+  parser.add_argument("X",
+                      type=float,
+                      help="Base of the right triangle; outward extent of the arm.")
+  parser.add_argument("Y",
+                      type=float,
+                      help="Height of the right triangle; upward extent of the arm.")
+  parser.add_argument("L1",
+                      type=float,
+                      help="Length of the upper arm (shoulder to elbow) in inches.")
+  parser.add_argument("L2",
+                      type=float,
+                      help="Length of the forearm (elbow to wrist), again in inches.")
+  parser.add_argument("-v",
+                      "--verbose",
+                      type=bool,
+                      default=False,
+                      help="If supplied, causes this script to print more verbose messages.")
+  args = parser.parse_args()
 
-    parser.parse_args()
+  verbosity = args.verbose
 
-    verbosity = parser.verbose
-
-    return parser.L1, parser.L2, parser.X, parser.Y
+  return args.L1, args.L2, args.X, args.Y
 
 
 if __name__ == "__main__":
-    L1, L2, X, Y = parseArguments()
+  RADIANS_TO_DEGREES = 180.0 / math.pi 
+  L1, L2, X, Y = parseArguments()
 
-    errorMessage = ""
+  errorMessage = ""
 
-    h = math.sqrt(X * X + Y * Y)
+  h = math.sqrt(X * X + Y * Y)
 
-    if X < 0:
-        errorMessage = "Illegal negative value for X"
-    elif Y < 0:
-        errorMessage = "Illegal negative value for Y"
-    elif h < math.abs(L2 - L1):
-        errorMessage = f"Impossible hypotenuse (elbow squeezed too tightly; cannot be less than {L2:.2f} - {L1:.2f} = {L2 - L1:.2f})"
-    elif h > L1 + L2:
-        errorMessage = "Impossible hypotenuse (arm stretched too far; cannot be greater than {L1:.2f} + {L2:.2f})"
+  if X < 0:
+    errorMessage = "Illegal negative value for X"
+  elif Y < 0:
+    errorMessage = "Illegal negative value for Y"
+  elif h < math.fabs(L2 - L1):
+    errorMessage = f"Impossible hypotenuse (elbow squeezed too tightly; cannot be less than {L2:.2f} - {L1:.2f} = {L2 - L1:.2f})"
+  elif h > L1 + L2:
+    errorMessage = f"Impossible hypotenuse (arm stretched too far; cannot be greater than {L1:.2f} + {L2:.2f})"
 
-    if errorMessage != "":
-        print(
-            f"Illegal argument detected.  Error message was:\n  {errorMessage}")
-        exit(1)
+  if errorMessage != "":
+    print(f"Extremely Illegal argument detected.  Error message was:\n  {errorMessage}")
+    exit(1)
 
-    # If control makes it here, all values are okay.
+  # If control makes it here, all values are okay.
+  gamma = math.acos((h * h - L1 * L1 - L2 * L2) / (-2 * L1 * L2))
+  alpha = math.asin(L2 * math.sin(gamma) / h)
+  beta = math.pi - alpha - gamma
+  theta = math.asin(Y / h)
+  phi = math.asin(X / h)
+  
+  shoulderAngle = (alpha + theta) * RADIANS_TO_DEGREES
+  elbowAngle = gamma * RADIANS_TO_DEGREES
+  wristAngle = (beta + phi) * RADIANS_TO_DEGREES
+  
+  if verbosity:
+    print(f"""h = {h} inches
+ɣ = {gamma} radians
+ɑ = {alpha} radians
+β = {beta} radians
+θ = {theta} radians
+φ = {phi} radians
+
+shoulder = ɑ + θ = {shoulderAngle}° (from horizontal)
+elbow    = ɣ     = {elbowAngle}° (bending outward)
+wrist    = β + φ = {wristAngle}° (from vertical, pointing down)
+""")
+  else:
+    print(f"""shoulder = {shoulderAngle:.2f}° (from horizontal)
+elbow    = {elbowAngle:.2f}° (bending outward)
+wrist    = {wristAngle:.2f}° (from pointing up)
+""")
