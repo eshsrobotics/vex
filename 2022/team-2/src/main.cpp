ce224751f68658adcee112ae9ef8ea80b49fc600
mgookin24 <matthewgookin@gmail.com>
2023-03-30 17:56:30 -0700

    We added a new project,titled "team-2".

---
+++ 2022/team-2/src/main.cpp
@@ -0,0 +1,21 @@
+/*----------------------------------------------------------------------------*/
+/*                                                                            */
+/*    Module:       main.cpp                                                  */
+/*    Author:       C:\Users\eshsp                                            */
+/*    Created:      Thu Mar 30 2023                                           */
+/*    Description:  V5 project                                                */
+/*                                                                            */
+/*----------------------------------------------------------------------------*/
+
+// ---- START VEXCODE CONFIGURED DEVICES ----
+// ---- END VEXCODE CONFIGURED DEVICES ----
+
+#include "vex.h"
+
+using namespace vex;
+
+int main() {
+  // Initializing Robot Configuration. DO NOT REMOVE!
+  vexcodeInit();
+  
+}
