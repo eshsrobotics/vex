7b863191a706de9875ff32b0df787efbe08955a1
fireblaze2000 <mary.amara0307@gmail.com>
2023-03-30 19:27:05 -0700

    We created the Mecanum drive.
    - https://github.com/FTC7393/EVLib/wiki/Mecanum-Wheels gave us the
    formula to create the code to drive.
    - The left side had to be reversed.
    - the line tracking sensor needs to be programed.
    - The camera needs to be programed.
    - Cable managment.

--- 2022/team-2/include/robot-config.h
+++ 2022/team-2/include/robot-config.h
@@ -2,9 +2,26 @@ using namespace vex;
 
 extern brain Brain;
 
+using signature = vision::signature;
+
+// VEXcode devices
+extern motor BackRight;
+extern motor FrontLeft;
+extern motor FrontRight;
+extern motor BackLeft;
+extern signature VisionSensor__SIG_1;
+extern signature VisionSensor__SIG_2;
+extern signature VisionSensor__SIG_3;
+extern signature VisionSensor__SIG_4;
+extern signature VisionSensor__SIG_5;
+extern signature VisionSensor__SIG_6;
+extern signature VisionSensor__SIG_7;
+extern vision VisionSensor;
+extern controller Controller1;
+
 /**
  * Used to initialize code/tasks/devices added using tools in VEXcode Pro.
- *
+ * 
  * This should be called at the start of your int main function.
  */
-void vexcodeInit(void);
+void  vexcodeInit( void );
--- 2022/team-2/src/main.cpp
+++ 2022/team-2/src/main.cpp
@@ -8,14 +8,44 @@
 /*----------------------------------------------------------------------------*/
 
 // ---- START VEXCODE CONFIGURED DEVICES ----
+// Robot Configuration:
+// [Name]               [Type]        [Port(s)]
+// BackRight            motor         10              
+// FrontLeft            motor         11              
+// FrontRight           motor         1               
+// BackLeft             motor         12              
+// VisionSensor         vision        2               
+// Controller1          controller                    
 // ---- END VEXCODE CONFIGURED DEVICES ----
 
 #include "vex.h"
 
 using namespace vex;
+void drive(double forwardBack, double leftRight, double rotate) {
+  //   inputs: x, y, and r
+
+  // flPower = + x + y + r
+  // frPower = + x - y - r
+  // blPower = + x - y + r
+  // brPower = + x + y - r
+  FrontLeft.setVelocity(forwardBack + leftRight + rotate, percent);
+  FrontLeft.spin(forward);
+  FrontRight.setVelocity(forwardBack - leftRight - rotate, percent);
+  FrontRight.spin(forward);
+  BackLeft.setVelocity(forwardBack - leftRight + rotate, percent);
+  BackLeft.spin(forward);
+  BackRight.setVelocity(forwardBack + leftRight - rotate, percent);
+  BackRight.spin(forward);
+} 
 
 int main() {
   // Initializing Robot Configuration. DO NOT REMOVE!
   vexcodeInit();
-  
+  while (true) {
+    drive(Controller1.Axis3.position(percent), 
+          Controller1.Axis4.position(percent),
+          Controller1.Axis1.position(percent));
+
+    wait(50, msec);
+  }
 }
