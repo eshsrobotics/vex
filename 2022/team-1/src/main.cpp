b66fa5b0102471343bc26c9c2444fb89dfd274b7
Vdavis_26 <vincentdavis910@gmail.com>
2023-05-10 19:30:45 -0700

    We worked on line tracker code.
    - Functrion that given linetracker valuses can tell if it is tape or
      not.
    - Started to rewrite main loop so there is a process of comand so
      linetrackers can overide all other movement commands.
    - Line tracker function was started, we are going to deal with 8 out of
      the 16 cases, all the cases when there is a vvalue on the same side or
      there is a value in one corner.
    - Still have to make it so teleop can minipulate the 3 variables that
      are used to drive and color hater needs to be able to override teleop.

--- 2022/team-1/src/main.cpp
+++ 2022/team-1/src/main.cpp
@@ -28,34 +28,62 @@
 
 const double AVERAGE_FLOOR_VALUE = 59.5;
 const double FLOOR_SPREAD = 8.5;
-const double AVRAGE_NEW_TAPE_VALUE = 77.5;
+const double AVERAGE_NEW_TAPE_VALUE = 77.5;
 const double NEW_TAPE_SPREAD = 15.5;
 const double AVERAGE_OLD_TAPE_VALUE = 63;
 const double OLD_TAPE_SPREAD = 10;
+const int STRAFE_SPEED_PERCENT = 30;
 
-double FloorPercentage (double lineSensorReading) {
+double floorPercentage (double lineSensorReading) {
   const double min = AVERAGE_FLOOR_VALUE - FLOOR_SPREAD;
   const double max = AVERAGE_FLOOR_VALUE + FLOOR_SPREAD;
   const double u = (lineSensorReading - min)/(max - min);
   return u;
 }
 
+double newTapePercentage (double lineSensorReading) {
+  const double min = AVERAGE_NEW_TAPE_VALUE - NEW_TAPE_SPREAD;
+  const double max = AVERAGE_NEW_TAPE_VALUE + NEW_TAPE_SPREAD;
+  const double u = (lineSensorReading - min)/(max - min);
+  return u;
+}
+
+// Checks if the value given by the linetrackers is tape, floor, both, or neither.
+bool isTape (double lineSensorReading) {
+  const double uFloor = floorPercentage(lineSensorReading);
+  const double uNewTape = newTapePercentage(lineSensorReading);
+  if (uFloor > 0 && uFloor < 1 && uNewTape > 0 && uNewTape < 1) {
+    // When the value falls into both the tape and floor range then we say that it is tape.
+    // This is to be extra careful because false negatives (mistake tape for floor) are more 
+    // detrimental then false positives (mistaking floor for tape).
+    return true;
+  } else if (uFloor > 0 && uFloor < 1) {
+    // If it falls into the floor values then it is the floor and it cannot be in the tape value.
+    return false;
+  } else if (uNewTape > 0 && uNewTape < 1) {
+    // If it falls into the new tape values then it is the tape and it cannot be in the floor value.
+    return true;
+  } else {
+    // If a value that is not seen as tape or floor it will default to floor.
+    return false;
+  }
+}
+
+
+
 using namespace vex;
 
 signature hatedCube = VisionSensor__LIKED_CUBE;
 
-// Drive is a way to call mechanum so both teliop and autonomus can use it.
-// fowardBack is the amount of movement in the foward and back channel a
-// positive
-// value drives foward a negative drives backwards and 0 stops it.
-// rotate is the amount of movement in the turning right and left channel, as
-// with
-// fowardBack; positive value drives foward a negative drives backwards and 0
-// stops it.
-// strafe is the amount of movement in the left and rivht channel a positive
-// value drives foward a negative drives backwards and 0 stops it.
-
-/*void drive(double fowardBack, double rotate, double strafe) {
+// Drive is a way to call mechanum so both teleop and autonomous can use it.
+// * forwardBack is the amount of movement in the forward and back channel; a
+//   positive value drives forward, a negative drives backwards, and 0 stops it.
+// * rotate is the amount of clockwise or counterclockwise rotation, as
+//   with fowardBack; positive value drives clockwise, a negative turns counterclockwise,
+//   and 0 stops it.
+// * strafe is the amount the robot moves directly right or left a positive
+//   value drives right, a negative drives left, and 0 stops it.
+void drive(double fowardBack, double rotate, double strafe) {
   leftFront.setVelocity(fowardBack + rotate + strafe, percent);
   rightFront.setVelocity(fowardBack - rotate - strafe, percent);
   leftBack.setVelocity(fowardBack + rotate - strafe, percent);
@@ -73,7 +101,7 @@ void colorHater(int fowardBack, int rotate, int strafe) {
   }else {
     LED_RED.off();
   }
-}*/
+}
 
 // This function should be called when VisionSensor.largestObject.exists is
 // true.
@@ -121,16 +149,19 @@ void detectLikedCube(int Xcenter, int Ycenter, int OKErrorX, int OKErrorY) {
 }
 
 // Looks arround in a clockwise direction looking for the target.
-void lookAround() {
-  leftFront.spinFor(
-      forward, 30, degrees,
-      false); // False makes the robot not wait for the robot to reach teh
-              // desired position to continue with the code
-  leftBack.spinFor(forward, 30, degrees, false);
-  rightFront.spinFor(reverse, 30, degrees, false);
-  rightBack.spinFor(reverse, 30,
-                    degrees); // No false means code will not continue until
-                              // robot reaches desired position
+void lookAround(int& forwardBack, int& rotate, int& strafe) {
+  // leftFront.spinFor(
+  //     forward, 30, degrees,
+  //     false); // False makes the robot not wait for the robot to reach teh
+  //             // desired position to continue with the code
+  // leftBack.spinFor(forward, 30, degrees, false);
+  // rightFront.spinFor(reverse, 30, degrees, false);
+  // rightBack.spinFor(reverse, 30,
+  //                   degrees); // No false means code will not continue until
+  //                             // robot reaches desired position
+  forwardBack = 0;
+  rotate = 30; // 30% rotation speed.
+  strafe = 0;
   cameraMotor.spinToPosition(30, degrees);
   Brain.Screen.setCursor(2, 1);
   Brain.Screen.print("trying to look around");
@@ -145,6 +176,7 @@ void BackUp() {
   rightBack.spinFor(reverse, BackUpDistance, degrees);
 }
 
+// Used for calibrating linetrackers
 void lineTrackerInfo() {
   int fr = FrontRightTracker.reflectivity(pct);
   int fl = FrontLeftTracker.reflectivity(pct);
@@ -155,14 +187,6 @@ void lineTrackerInfo() {
   const int screenY = 3;
   Brain.Screen.setCursor(screenY, screenX);
   Brain.Screen.print("FR: %02d, FL: %02d, BR: %02d, BL: %02d", fr, fl, br, bl);
-  VisionSensor.takeSnapshot(VisionSensor__DISLIKED_CUBE);
-  while (((FrontRightTracker.reflectivity() < 63) ||
-          (FrontLeftTracker.reflectivity() < 63) ||
-          (BackRightTracker.reflectivity() < 63) ||
-          (BackLeftTracker.reflectivity() < 63)) &&
-         (DistanceSensor.objectDistance(mm)) < 1000) {
-    VisionSensor.takeSnapshot(VisionSensor__DISLIKED_CUBE);
-  }
 }
 
 void MoveUp() {
@@ -181,24 +205,34 @@ void DistanceInfo() {
   Brain.Screen.print("%01f", DistanceSensor.objectDistance(mm));
 }
 
+void lineTrackerInfo(int& forwardBack, int& rotate, int& strafe) {
+  bool blTape = isTape(BackLeftTracker.reflectivity());
+  bool brTape = isTape(BackRightTracker.reflectivity());
+  bool flTape = isTape(FrontLeftTracker.reflectivity());
+  bool frTape = isTape(FrontRightTracker.reflectivity());  
+
+  if (blTape && flTape) {
+    // Tape on the left side.
+    strafe = STRAFE_SPEED_PERCENT;
+  }    
+}
 
 struct VisionTarget {
   double x;
   double y;
   double distanceFeet;
   bool cubeExists;
-}
+};
 
-void trackColor(signature colorSignature, ){
-  if(VisionSensor.objectCount() == 0){
-    
-  }
+// void trackColor(signature colorSignature, ){
+//   if(VisionSensor.objectCount() == 0){  
+//   }
 
 int main() {
   // Initializing Robot Configuration. DO NOT REMOVE!
   vexcodeInit();
 
-  int fowardBack;
+  int forwardBack;
   int rotate;
   int strafe;
   int x = 0;
@@ -235,7 +269,14 @@ int main() {
       }
 
     } else {
-      lookAround();
+      // Only do this if no other actions.
+      lookAround(forwardBack, rotate, strafe);
     }
+
+    // A fail safe so if there is tape detected then the forwardBack, rotate, and strafe values will be altered.
+    lineTrackerOverride(forwardBack, rotate, strafe);
+
+    // Drive the chassis according to the final decision
+    drive(forwardBack, rotate, strafe);
   }
 }
