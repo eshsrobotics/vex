7193a8aeb2d868ac8ee04a9bff63f8469f4b7d95
mgookin24 <matthewgookin@gmail.com>
2023-11-09 17:12:17 -0800

    Added code for the task tree and a wait task
    - Used code from 2021's task tree for the framework of the task tree and
      a task to wait a specified number of milliseconds
      - Still need to make a drive task and turn task
      - We will make an autonomous plan first, then will move that into the
        code

---
+++ 2023/Team-A/Team-A-auton-task-tree/src/auton_task_tree.cpp
@@ -0,0 +1,123 @@
+#include "auton_task_tree.h"
+#include "vex.h"
+#include <cstring> // std::snprintf()
+#include <list>
+#include <string>
+
+
+using namespace vex;
+using namespace std;
+
+/*--------------------------------------------------------*/
+/*                    Task Methods                        */
+/*--------------------------------------------------------*/
+
+Task::Task(const string &name) : name(name) {
+  static int counter = 0;
+  task_id = ++counter;
+
+  // Append the (unique) task ID to the name of the task.
+  const size_t MAX_DIGITS = 10;
+  char buffer[MAX_DIGITS];
+  snprintf(buffer, sizeof(buffer), "%d", task_id);
+  this->name += "#";
+  this->name += buffer;
+}
+
+void addTask(std::shared_ptr<Task> parentTask,
+             std::shared_ptr<Task> childTask) {
+  parentTask->children.push_back(childTask);
+  childTask->parents.push_back(parentTask);
+}
+
+void execute(std::shared_ptr<Task> rootTask) {
+  // List of tasks currently running
+  std::list<shared_ptr<Task>> taskList;
+
+  // Start running the task tree
+  taskList.push_back(rootTask);
+  rootTask->start();
+
+  while (!taskList.empty()) {
+    // Check to see if any of the running tasks have finished
+    for (auto iter = taskList.begin(); iter != taskList.end();
+         /* See increment below */) {
+      auto task = *iter;
+      if (task->done()) {
+        // Put the task's children into the list if they're ready (i.e., when
+        // all its parents are completed)
+        for (auto childTask : task->children) {
+          bool childTaskReady = true;
+          for (auto parentTask : childTask->parents) {
+            // If the parent task doesn't exist, it is completed, so the child
+            // task won't wait for it
+            if (parentTask.expired()) {
+              continue;
+            }
+
+            // If one of our parents is running, then we are not ready.
+            if (!parentTask.lock()->done()) {
+              childTaskReady = false;
+              break;
+            }
+          } // end (for each parent of the current child task)
+
+          // If all the parents are completed or destroyed, the task will
+          // start
+          if (childTaskReady) {
+            taskList.push_back(childTask);
+            childTask->start();
+          }
+        } // end (for each child of the currently ending task)
+
+        // Remove the original task from the list
+        // This is safe if the task is a shared parent of a child task that
+        // currently isn't ready
+        iter = taskList.erase(iter);
+      } else {
+        // The current task is still running.  Move to the next task in the
+        // list.
+        ++iter;
+      }
+    } // end (for each active task)
+
+    wait(15, msec);
+  } // end (while there are still active tasks)
+}
+
+/*--------------------------------------------------------*/
+/*            WaitMillisecondsTask Methods                */
+/*--------------------------------------------------------*/
+
+WaitMillisecondsTask::WaitMillisecondsTask(double milliseconds)
+    : Task("Wait task") {
+  waitPeriodMilliseconds = milliseconds;
+}
+
+bool WaitMillisecondsTask::done() const {
+  if (Brain.timer(msec) >= doneTimeMilliseconds) {
+    return true;
+  } else {
+    return false;
+  }
+}
+
+void WaitMillisecondsTask::start() {
+  startTimeMilliseconds = Brain.timer(msec);
+  doneTimeMilliseconds = startTimeMilliseconds + waitPeriodMilliseconds;
+}
+
+std::shared_ptr<Task> get_auton(AUTON_TYPE type) {
+
+  auto wait0 = std::shared_ptr<Task>(new WaitMillisecondsTask(0));
+  
+  // The root task always needs to be wait0
+
+  switch(type) {
+    case TEST_AUTON:
+      break;
+
+  }
+
+  return wait0;
+}
