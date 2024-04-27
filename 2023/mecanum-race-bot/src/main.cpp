4d4f9917318cfa9cbf46ec1ab434923b9c5c54b4
mgookin24 <matthewgookin@gmail.com>
2024-04-26 18:47:25 -0700

    Finished testing and made some final modifications to the mecanum drive code
    -Fixed problems involving the motors firing in different directions
    -Realized that Vex's poor design came back to bite us. All the port
    assignments were off by one.
    -Also made some hardware changes due to shaft collars interfering with
    the wheels engaging
    -The motor on port 5 (the physical port 5) kept disconnecting. Giving the
    cable some more slack helped, but definitely something to look out for.

--- 2023/mecanum-race-bot/src/main.cpp
+++ 2023/mecanum-race-bot/src/main.cpp
@@ -9,18 +9,20 @@
 #include "vex.h"
 
 using namespace vex;
-const int FRONT_LEFT_PORT = 5;
-const int FRONT_RIGHT_PORT = 2;
-const int BACK_LEFT_PORT = 3;
-const int BACK_RIGHT_PORT = 6;
+const int FRONT_LEFT_PORT = 4;
+const int FRONT_RIGHT_PORT = 1;
+const int BACK_LEFT_PORT = 2;
+const int BACK_RIGHT_PORT = 5;
+
+//We really oughtta start not making that port numbering mistake (the port numbers were all one too high thanks to Vex design)...
 
 //When the robot is oriented so the brain is on the left side, the robot is facing forward
 
 vex::controller Controller;
 vex::motor front_left(FRONT_LEFT_PORT);
 vex::motor front_right(FRONT_RIGHT_PORT);
-vex::motor back_left(BACK_LEFT_PORT);
-vex::motor back_right(BACK_RIGHT_PORT);
+vex::motor back_left(BACK_LEFT_PORT, true);
+vex::motor back_right(BACK_RIGHT_PORT, true);
 
 void mecanum_drive(int front_back_percent, int left_right_percent, int turn_percent) {
     front_left.spin(directionType::fwd, front_back_percent + turn_percent + left_right_percent, percentUnits::pct);
@@ -45,7 +47,10 @@ int main() {
         int left_right_speed = Controller.Axis3.position();
         int turn_speed = Controller.Axis1.position();
         mecanum_drive(forward_back_speed, left_right_speed, turn_speed);
-        
+        //front_left.spin(fwd);
+        //front_right.spin(fwd);
+        //back_left.spin(fwd);
+        //back_right.spin(fwd);
         // Allow other tasks to run
         this_thread::sleep_for(10);
     }
