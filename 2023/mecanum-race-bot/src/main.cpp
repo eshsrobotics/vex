1f200ebc18b6c7d01e7fd609b2abd29fc151f984
Uche Akotaobi <uche.robotics@akotaobi.org>
2024-04-27 13:09:46 -0700

    Corrected the control scheme for the Mecanum drive.
    - 2023/mecanum-race-bot/src/main.cpp:
      * (Global scope):
        1. Make it clear which physical ports
           correspond to the virtual ports the motors
           are attached to.
        2. Correct the motor reversal flags based on
           the changes to the control schemes.  When
           we have all four motors driving forward
           under the new scheme, we need front_right
           and back_right to be reversed rather than
           front_left and back_right.
      * (main):
        1. Remove dead code.
        2. Important bugfix: forward_back_speed should
           have been connected to the vertical channel
           of the left joystick.  The previous commits
           accidentally assigned forward_back_speed to
           the *horizontal* channel of the left
           joystick, and then reversed motors to make
           this scheme drive forward anyway.
    
           This confused us for hours, and was not
           easy to spot.

--- 2023/mecanum-race-bot/src/main.cpp
+++ 2023/mecanum-race-bot/src/main.cpp
@@ -9,10 +9,11 @@
 #include "vex.h"
 
 using namespace vex;
-const int FRONT_LEFT_PORT = 4;
-const int FRONT_RIGHT_PORT = 1;
-const int BACK_LEFT_PORT = 2;
-const int BACK_RIGHT_PORT = 5;
+const int FRONT_LEFT_PORT = 5 - 1;
+const int FRONT_RIGHT_PORT = 2 - 1;
+const int BACK_LEFT_PORT = 3 - 1;
+const int BACK_RIGHT_PORT = 6 - 1;
+
 
 //We really oughtta start not making that port numbering mistake (the port numbers were all one too high thanks to Vex design)...
 
@@ -20,8 +21,8 @@ const int BACK_RIGHT_PORT = 5;
 
 vex::controller Controller;
 vex::motor front_left(FRONT_LEFT_PORT);
-vex::motor front_right(FRONT_RIGHT_PORT);
-vex::motor back_left(BACK_LEFT_PORT, true);
+vex::motor front_right(FRONT_RIGHT_PORT, true);
+vex::motor back_left(BACK_LEFT_PORT);
 vex::motor back_right(BACK_RIGHT_PORT, true);
 
 void mecanum_drive(int front_back_percent, int left_right_percent, int turn_percent) {
@@ -36,21 +37,12 @@ vex::brain       Brain;
 
 int main() {
 
-    // double velocity = 50;
-    // front_left.setVelocity(velocity, pct);
-    // front_right.setVelocity(velocity, pct);
-    // back_left.setVelocity(velocity, pct);
-    // back_right.setVelocity(velocity, pct);
-   
     while (true) {
-        int forward_back_speed = Controller.Axis4.position();
-        int left_right_speed = Controller.Axis3.position();
+        int forward_back_speed = Controller.Axis3.position();
+        int left_right_speed = Controller.Axis4.position();
         int turn_speed = Controller.Axis1.position();
         mecanum_drive(forward_back_speed, left_right_speed, turn_speed);
-        //front_left.spin(fwd);
-        //front_right.spin(fwd);
-        //back_left.spin(fwd);
-        //back_right.spin(fwd);
+
         // Allow other tasks to run
         this_thread::sleep_for(10);
     }
