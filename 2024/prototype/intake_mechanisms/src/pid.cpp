edfc17bd1a84dc72910c79c50101f56e075ae584
sepehrnik7gmailcom <sepehrnik7@gmail.com>
2024-11-17 18:51:00 -0800

    Fully implemented a PID controller.
    * A documentation is still needed for preventing integral windup.

---
+++ 2024/prototype/intake_mechanisms/src/pid.cpp
@@ -0,0 +1,25 @@
+#include "pid.h"
+#include <cmath> // fabs
+
+PidController::PidController(const double P_, const double I_, const double D_) 
+    : integral(0), P(P_), I(I_), D(D_) {}
+
+double PidController::calculate(const double measurement, const double setpoint) {
+    double error = setpoint - measurement;
+    integral = integral + error;
+    
+    
+    const double epsilon = 0.005;
+    bool reachedSetPoint = (fabs(error) <= epsilon);
+
+    // Trying to prevent integral wind-up, 
+    // When the error is greater than the setPoint, we are worried
+    bool errorIsTooLarge = (fabs(error) > fabs(setpoint));
+    if (reachedSetPoint || errorIsTooLarge) {
+        integral = 0;
+    }
+
+
+
+    previousError = error;
+}
