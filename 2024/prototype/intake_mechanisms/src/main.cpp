6b09f7b05d4f5d86cf10a7d8600bd9c40769638e
sepehrnik7gmailcom <sepehrnik7@gmail.com>
2024-12-29 19:59:12 -0800

    Fixed minor constructor build errors in previous commit.

--- 2024/prototype/intake_mechanisms/src/main.cpp
+++ 2024/prototype/intake_mechanisms/src/main.cpp
@@ -176,20 +176,20 @@ void autonomous() {
   auto rootTask = make_shared<WaitMillisecondsTask>(0);
   auto B = make_shared<DriveStraightTask>(-0.4572 * 100, prototype);
   auto C = make_shared<MobileGoalIntakeTask>(prototype, true);
-  auto D = make_shared<TurnTask>(94.7, gyro);
+  auto D = make_shared<TurnTask>(94.7, gyro, prototype);
   auto E = make_shared<DriveStraightTask>(0.22 * 100, prototype);
   auto F = make_shared<IntakeMillisecondsTask>(prototype, 1e5);
   auto G = make_shared<IntakeMillisecondsTask>(prototype, 67.96);
   auto H = make_shared<DriveStraightTask>(0.2 * 100, prototype);
-  auto I = make_shared<TurnTask>(65.01, gyro);
+  auto I = make_shared<TurnTask>(65.01, gyro, prototype);
   auto J = make_shared<MobileGoalIntakeTask>(prototype, false);
   auto K = make_shared<DriveStraightTask>(1.37 * 100, prototype);
   auto L = make_shared<IntakeMillisecondsTask>(prototype, 0.5);
-  auto M = make_shared<TurnTask>(8.57, gyro);
+  auto M = make_shared<TurnTask>(8.57, gyro, prototype);
   auto N = make_shared<DriveStraightTask>(-0.225 * 100, prototype);
   auto O = make_shared<MobileGoalIntakeTask>(prototype, true);
   auto P = make_shared<IntakeMillisecondsTask>(prototype, 1e5);
-  auto Q = make_shared<TurnTask>(prototype, -3.83);
+  auto Q = make_shared<TurnTask>(-3.83, gyro, prototype);
   auto R = make_shared<DriveStraightTask>(0.236 * 100, prototype);
 
   // Leg 1: Moving backwards to the first Mobile Goal.
