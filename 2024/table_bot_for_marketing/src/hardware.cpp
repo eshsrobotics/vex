2914dab1bd9acbd9c7e5c74e64f65ecacfb5e0e1
PlatinumWolf128 <sanjay.nataraj2009@gmail.com>
2025-04-20 13:43:43 -0700

    Created a new VEX Project to house the code for the marketing bot
    - Team P is making a table-bot for marketing, and
      this is where all the code for that will be

---
+++ 2024/table_bot_for_marketing/src/hardware.cpp
@@ -0,0 +1,8 @@
+#include "vex.h"
+#include "hardware.h"
+
+/*
+
+Here we'll put all of our definitions for the hardware, functions, etc
+
+*/
---
+++ 2024/table_bot_for_marketing/src/main.cpp
@@ -0,0 +1,20 @@
+/*----------------------------------------------------------------------------*/
+/*                                                                            */
+/*    Module:       main.cpp                                                  */
+/*    Author:       sanjaynataraj                                             */
+/*    Created:      4/20/2025, 1:39:17 PM                                     */
+/*    Description:  V5 project                                                */
+/*                                                                            */
+/*----------------------------------------------------------------------------*/
+#include "vex.h"
+
+using namespace vex;
+
+int main() {
+   
+    while(1) {
+        
+        // Allow other tasks to run
+        this_thread::sleep_for(10);
+    }
+}
