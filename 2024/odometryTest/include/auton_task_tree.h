69b2a4675f4ff77ae42411351d80b6331f008081
RushilGhosh <rushilghosh@gmail.com>
2025-12-10 16:18:37 -0800

    Began auton implementation
    - Created the task struct

---
+++ 2024/odometryTest/include/auton_task_tree.h
@@ -0,0 +1,64 @@
+#ifndef AUTON_TASK_TREE_H_INCLUDED
+#define AUTON_TASK_TREE_H_INCLUDED
+
+#include <functional>
+#include <memory> // shared_ptr<T>
+#include <string>
+#include <vector>
+#include "vex.h"
+
+/*--------------------------------------------------------------------------*/
+/*     The struct task is how our whole autonomous code runs, each task     */
+/* has one function that it runs for autonomous, and each task has children */
+/*               and parents which run before and after it.                 */
+/*--------------------------------------------------------------------------*/
+struct Task {
+  // Generates the unique task ID
+  Task(const std::string &name);
+
+  /*--------------------------------------------------------*/
+  /*                     Member data                        */
+  /*--------------------------------------------------------*/
+
+  // Task name, not unique whatsoever
+  std::string name;
+
+  // Task ID, unique for every task
+  int task_id;
+
+  // The parent tasks for the current task, the task runs when all parents are
+  // finished
+  std::vector<std::weak_ptr<Task>> parents;
+
+  // The child tasks of the current task, run when the task is finished
+  std::vector<std::shared_ptr<Task>> children;
+
+  /*--------------------------------------------------------*/
+  /*                  Virtual Methods                       */
+  /*--------------------------------------------------------*/
+
+  // Returns true if the task is completed, false if not completed
+  virtual bool done() const = 0;
+
+  // Starts the current task
+  virtual void start() = 0;
+
+  /*--------------------------------------------------------*/
+  /*                  Public Functions                      */
+  /*--------------------------------------------------------*/
+
+  // Add a new or existing task as a child of the current task
+  friend void addTask(std::shared_ptr<Task> parentTask,
+                      std::shared_ptr<Task> childTask);
+
+  // Executes this task, all of its children, and so on, some tasks may run in
+  // parallel
+  friend void execute(std::shared_ptr<Task> rootTask);
+};
+
+extern void addTask(std::shared_ptr<Task> parentTask,
+                    std::shared_ptr<Task> childTask);
+
+extern void execute(std::shared_ptr<Task> rootTask);
+
+#endif
